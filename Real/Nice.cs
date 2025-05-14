f6ad1450fea1210df36a348daeae1ef4f2144fe3
Ali Diaa <85357930+AliDiaa0@users.noreply.github.com>
2025-05-14 17:12:02 +0300

    Add files via upload

---
+++ Real/Nice.cs
@@ -0,0 +1,12 @@
+using System;
+namespace App
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("DAMN! WHY ARE YOU HERE?!" + Environment.NewLine + "GET OUT!");
+            Console.ReadKey();
+        }
+    }
+}
