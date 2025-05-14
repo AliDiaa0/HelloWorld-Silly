28301db77f6bbe82a94da09beb73ceeab8504ff3
Ali Diaa <85357930+AliDiaa0@users.noreply.github.com>
2025-05-14 17:16:18 +0300

    Add files via upload

---
+++ Enhanced/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Why are you here?" << std::endl << "WHY ARE YOU HERE! THIS IS NOT THE MAIN CODE, KID!" << std::endl;
+    std::cin.get();
+    return 0;
+}
