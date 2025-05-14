b6ac133fb7598bce1bb50a6959f260e6f052c19e
Ali Diaa <85357930+AliDiaa0@users.noreply.github.com>
2025-05-14 16:24:40 +0300

    Delete HelloWorld directory

--- HelloWorld/include/include.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include "random.h"
-#include "memory_alloc.h"
-#include "hello.h"
--- HelloWorld/include/random.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "include.h"
-#define SEED 15 // We have fifteen functions for greetings!
-
-extern int random;
-void randomness(); int random_funcs();
