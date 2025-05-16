6d6a152413b6da483a00713faef1a020ee9a6609
Ali Diaa <85357930+AliDiaa0@users.noreply.github.com>
2025-05-16 17:06:30 +0300

    Delete Enhanced directory

--- Enhanced/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Why are you here?" << std::endl << "WHY ARE YOU HERE! THIS IS NOT THE MAIN CODE, KID!" << std::endl;
-    std::cin.get();
-    return 0;
-}
